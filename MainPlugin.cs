14bbec58a8e872f7def33cbf95eaa1c9681cb204
sig649 <sig7alista@gmail.com>
2025-10-11 13:28:33 +0900

    version num fix

--- MainPlugin.cs
+++ MainPlugin.cs
@@ -12,7 +12,7 @@ namespace s649_OsuiGet
     {
         public const string GUID = "s649_OsuiGet";
         public const string MOD_TITLE = "Osui Get";
-        public const string MOD_VERSION = "0.1.0.0";
+        public const string MOD_VERSION = "0.1.1.0";
     }
 
     [BepInPlugin(PluginDatas.GUID, PluginDatas.MOD_TITLE, PluginDatas.MOD_VERSION)]
